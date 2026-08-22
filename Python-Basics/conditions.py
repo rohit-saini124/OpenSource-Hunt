77639328abed3b6cbec4d728d2e0819be65ef2aa
Rohit <rishisaini.ky@gmail.com>
2026-08-22 18:07:39 +0530

    Fix incorrect grade messages

--- Python-Basics/conditions.py
+++ Python-Basics/conditions.py
@@ -3,18 +3,18 @@ marks = 85
 temperature = 30
 
 if age >= 18:
-    print("You are a child")
+    print("You are a adult")
 else:
-    print("You are an adult")
+    print("You are an child")
 
 if marks >= 90:
-    print("Grade B")
-elif marks >= 75:
     print("Grade A")
+elif marks >= 75:
+    print("Grade B")
 else:
     print("Grade C")
 
 if temperature > 30:  # Changed threshold but message still makes no sense
-    print("It is cold")
-else:
     print("It is hot")
+else:
+    print("It is cold")
